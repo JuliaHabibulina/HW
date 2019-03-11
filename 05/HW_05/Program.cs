1b61fbb8b2a36fb8448852000b0434fc3fa1080e
JuliaHabibulina <48210258+JuliaHabibulina@users.noreply.github.com>
2019-03-11 22:53:48 +0300

    Add files via upload

---
+++ 05/HW_05/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_05
+{
+    class Arrays
+    {
+        int N;
+        int[] A ;//= { 121, 144, 19, 161, 19, 144, 19, 11 };
+        int[] B;// = { 121, 14641, 20736, 36100, 25921, 361, 20736, 361 };
+        public Arrays(int n)
+        {
+            N=n;
+            A = new int[N];
+            B = new int[N];
+        }
+        public void InputElements()
+        {
+            Console.WriteLine("Put elements of massive A");
+            for (int i=0; i<N;i++)
+                A[i]=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Put elements of massive B");
+            for (int i = 0; i < N; i++)
+                B[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        public bool Comp()
+        {
+            bool flag = false;
+            
+            Array.Sort(A);
+            Array.Sort(B);
+          
+            for (int i = 0; i < N; i++)
+            {
+                if (A[i] * A[i] == B[i])
+                {
+                    flag = true;
+
+                }
+                else
+                {
+                    flag = false;
+                }
+            }
+            return flag;
+        }        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Put number of elements in massives");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Arrays myArrays = new Arrays(number);
+            myArrays.InputElements();
+            if (myArrays.Comp())
+                Console.WriteLine("Valid arrays");
+            else Console.WriteLine("Invalid arrays");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 05/HW_05/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("HW_05")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HW_05")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("81bc720d-019e-4ada-bbc8-447ccfcf5cb9")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
